faab76eb3c6a4c1b4813cfa95aea347a7cfa4811
Ulrich Bodenhofer <bodenhofer@bioinf.jku.at>
2011-03-18 08:52:14 +0000

    simplified interface of plot()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/procoil@53864 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -1,7 +1,18 @@
 setMethod("plot", signature(x="CCProfile", y="missing"),
     function(x, col="red", rng=0, standardize=FALSE, shades=NULL,
-             legend.show=FALSE, text="", xpos="topright", ypos=NULL, ...)
+             legend="", legend.pos="topright", ...)
     {
+        if (!is.numeric(try(col2rgb(col), silent=TRUE)[1]))
+            stop("argument `col' must be a string denoting a color\n")
+
+        if (!is.null(shades) &&
+            (!is.numeric(try(col2rgb(shades[1]), silent=TRUE)[1]) ||
+             !is.numeric(try(col2rgb(shades[2]), silent=TRUE)[1])))
+            stop("argument `shades' must be a vector of two colors\n")
+
+        if (!is.null(legend) && !is.character(legend))
+            stop("argument `legend' must be a string\n")
+
         n <-nchar(x@seq)
 
         prfl <- x@profile
@@ -61,18 +72,37 @@ setMethod("plot", signature(x="CCProfile", y="missing"),
         lines(c(1:(n + 1)), c(prfl, prfl[n]),
               type="s", col=col)
 
-        if (legend.show)
-            legend(x=xpos, y=ypos, col=c(col),
-                legend=c(text), lwd=1, bg="white")
+        if (length(legend) == 1 && legend != "")
+        {
+            if (length(legend.pos) > 1)
+                legend(x=legend.pos[1], y=legend.pos[2], col=col,
+                       legend=legend, lwd=1, bg="white")
+            else
+                legend(x=legend.pos[1], col=col,
+                       legend=legend, lwd=1, bg="white")
+        }
     }
 )
 
 
 setMethod("plot", signature(x="CCProfile", y="CCProfile"),
-    function(x, y, xcol="red", ycol="blue", rng=0,
-             standardize=FALSE, shades=NULL, legend.show=FALSE,
-             xtext="", ytext="", xpos="topright", ypos=NULL, ...)
+    function(x, y, col=c("red", "blue"), rng=0,
+             standardize=FALSE, shades=NULL, legend=NULL,
+             legend.pos="topright", ...)
     {
+        if (!is.null(col) &&
+            (!is.numeric(try(col2rgb(col[1]), silent=TRUE)[1]) ||
+             !is.numeric(try(col2rgb(col[2]), silent=TRUE)[1])))
+            stop("argument `col' must be a vector of two colors\n")
+
+        if (!is.null(shades) &&
+            (!is.numeric(try(col2rgb(shades[1]), silent=TRUE)[1]) ||
+             !is.numeric(try(col2rgb(shades[2]), silent=TRUE)[1])))
+            stop("argument `shades' must be a vector of two colors\n")
+
+        if (!is.null(legend) && (!is.character(legend) || length(legend) < 2))
+            stop("argument `legend' must be a vector of two strings\n")
+
         n <- nchar(x@seq)
         b <- x@b
 
@@ -149,21 +179,27 @@ setMethod("plot", signature(x="CCProfile", y="CCProfile"),
         matchS <- (xAASeq == yAASeq)
 
         mtext(side=1, at=(1:n + 0.5), line=0, text=yAASeq,
-              col=ifelse(matchS, "black", ycol))
+              col=ifelse(matchS, "black", col[2]))
         mtext(side=3, at=(1:n + 0.5), line=0, text=xAASeq,
-              col=ifelse(matchS, "black", xcol))
+              col=ifelse(matchS, "black", col[1]))
         text(1:n + 0.5, 0, strsplit(x@reg, "")[[1]], adj=c(0.5,0))
 
         prfl <- x@profile
         if (standardize) prfl <- prfl + b / n
-        lines(c(1:(n + 1)), c(prfl, prfl[n]), type="s", col=xcol)
+        lines(c(1:(n + 1)), c(prfl, prfl[n]), type="s", col=col[1])
 
         prfl <- y@profile
         if (standardize) prfl <- prfl + b / n
-        lines(c(1:(n + 1)), c(prfl, prfl[n]), type="s", col=ycol)
+        lines(c(1:(n + 1)), c(prfl, prfl[n]), type="s", col=col[2])
 
-        if (legend.show)
-            legend(x=xpos, y=ypos, col=c(xcol, ycol),
-                legend=c(xtext, ytext), lwd=1, bg="white")
+        if (!is.null(legend))
+        {
+            if (length(legend.pos) > 1)
+                legend(x=legend.pos[1], y=legend.pos[2], col=col,
+                       legend=legend, lwd=1, bg="white")
+            else
+                legend(x=legend.pos[1], col=col,
+                       legend=legend, lwd=1, bg="white")
+        }
     }
 )
